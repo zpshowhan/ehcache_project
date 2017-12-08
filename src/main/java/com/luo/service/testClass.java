984a9798516cd2fe340d49622305221dc8dbca52
雷雨 <1903373681@qq.com>
2017-12-08 17:24:41 +0800

    Update testClass.java

--- src/main/java/com/luo/service/testClass.java
+++ src/main/java/com/luo/service/testClass.java
@@ -1,25 +1,11 @@
-/**   
-* @Title: testClass.java 
-* @Package com.luo.service 
-* @Description: TODO(用一句话描述该文件做什么) 
-* @Company:方正
-* @author zhaolei  
-* @date 2017年6月30日 上午10:10:28 
-* @version V1.0   
-*/
+
 package com.luo.service;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-/** 
-* @ClassName: testClass 
-* @Description: TODO(这里用一句话描述这个类的作用) 
-* @Company:方正
-* @author zhaolei 
-* @version 1.0 2017年6月30日 上午10:10:28 
-*/
+
 public class testClass {
 
 	 public static void main(String[] args)    
